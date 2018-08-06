4b94773a6efcea8769a0774a9237b6b6ce0d0958
saikrishnadhulipalla <41868252+saikrishnadhulipalla@users.noreply.github.com>
2018-08-06 15:12:54 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner g = new Scanner (System.in);
+		int f = g.nextInt();
+		  for (int i = 0; i<f; i++)
+		   System.out.println("Hello");
+ 
+	}
+}
